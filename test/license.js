8a5a056ced7e52f63c0062c022452848d05733ff
Francesco Trotta <github@fasttime.org>
2024-11-23 06:37:40 +0100

    license test

---
+++ test/license.js
@@ -0,0 +1,16 @@
+import assert           from 'node:assert/strict';
+import { readFile }     from 'node:fs/promises';
+import eslintDirURL     from '../lib/default-eslint-dir-url.js';
+
+it
+(
+    'ESLint License',
+    async () =>
+    {
+        const actualURL = new URL('../ESLint License.txt', import.meta.url);
+        const expectedURL = new URL('./LICENSE', eslintDirURL);
+        const [actual, expected] =
+        await Promise.all([readFile(actualURL), readFile(expectedURL)]);
+        assert.deepEqual(actual, expected);
+    },
+);
